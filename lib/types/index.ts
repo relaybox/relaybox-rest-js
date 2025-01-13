38c3c976f1e3dbc1a7c22c9183a0eaaa21c96e32
livecloud-labs <livecloud.store@gmail.com>
2025-01-13 17:44:20 +0000

    chore: update tsconfig

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -3,3 +3,5 @@ export * from './jwt.types.js';
 export * from './permission.types.js';
 export * from './request.types.js';
 export * from './response.types.js';
+export * from './rooms.types.js';
+export * from './webhook.types.js';
