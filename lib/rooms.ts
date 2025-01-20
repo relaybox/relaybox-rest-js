5b75c658348faad656009504f935b8584af6cb25
livecloud-labs <livecloud.store@gmail.com>
2025-01-20 18:01:43 +0000

    chore: add member add and update member type methods

--- lib/rooms.ts
+++ lib/rooms.ts
@@ -1,11 +1,42 @@
 import { defaultHeaders, serviceRequest } from './request.js';
 import { generateAuthToken } from './signature.js';
 import { ApiKeyParts, ExtendedJwtPayload, HttpMethod } from './types/index.js';
-import { Room, RoomOptions, RoomTokenType } from './types/rooms.types.js';
+import { Room, RoomMemberType, RoomOptions, RoomTokenType } from './types/rooms.types.js';
 
 const STATE_SERVICE_PATHS = {
   rooms: '/rooms'
 };
+
+interface MemberActions {
+  /**
+   * Get paginated list of members.
+   * @param {PaginatedRequestOptions} opts The clientId of the member to add
+   * @example
+   * await room.members.get({ offset: 0, limit: 10 })
+   */
+  // get: (opts?: PaginatedRequestOptions) => Promise<PaginatedResponse<RoomMember>>;
+  /**
+   * Add member to private room. Private rooms only.
+   * @param {string} clientId The clientId of the member to add
+   */
+  add: (roomId: string, clientId: string, authToken: string) => Promise<void>;
+  /**
+   * Remove member from private room. Private rooms only.
+   * @param clientId The clientId of the member to delete
+   */
+  // remove: (clientId: string) => Promise<void>;
+  /**
+   * Set member type for client id.
+   * @param clientId The clientId of the member to delete
+   * @param memberType The member type to set for this user
+   */
+  setMemberType: (
+    roomid: string,
+    clientId: string,
+    memberType: RoomMemberType,
+    authToken: string
+  ) => Promise<void>;
+}
 
 export class Rooms {
   constructor(
@@ -77,6 +108,72 @@ export class Rooms {
 
     const requestUrl = `${this.stateServiceUrl}${STATE_SERVICE_PATHS.rooms}`;
 
+    return serviceRequest<Room>(requestUrl, requestParams);
+  }
+
+  /**
+   * Add a user to a private room by client id
+   * @param {string} roomId - The name of the room.
+   * @param {string} clientId - The client id of the user to add.
+   * @param {string} authToken - The authorization token.
+   * @returns
+   */
+  async addMember(roomId: string, clientId: string, authToken: string) {
+    const requestBody = {
+      roomId,
+      clientId
+    };
+
+    const requestParams = {
+      method: HttpMethod.POST,
+      headers: {
+        ...defaultHeaders,
+        Authorization: `Bearer ${authToken}`
+      },
+      body: JSON.stringify(requestBody)
+    };
+
+    const requestUrl = `${this.stateServiceUrl}${STATE_SERVICE_PATHS.rooms}/${roomId}/members`;
+
     return serviceRequest<any>(requestUrl, requestParams);
   }
+
+  /**
+   * Set member type for client id.
+   * @param {string} roomId - The name of the room.
+   * @param {string} clientId - The client id of the user to add.
+   * @param {RoomMemberType} memberType - The member type to set for this user
+   * @param {string} authToken - The authorization token.
+   * @returns
+   */
+  async setMemberType(
+    roomId: string,
+    clientId: string,
+    memberType: RoomMemberType,
+    authToken: string
+  ) {
+    const requestBody = {
+      roomId,
+      clientId,
+      memberType
+    };
+
+    const requestParams = {
+      method: HttpMethod.PUT,
+      headers: {
+        ...defaultHeaders,
+        Authorization: `Bearer ${authToken}`
+      },
+      body: JSON.stringify(requestBody)
+    };
+
+    const requestUrl = `${this.stateServiceUrl}${STATE_SERVICE_PATHS.rooms}/${roomId}/members/${clientId}`;
+
+    return serviceRequest<any>(requestUrl, requestParams);
+  }
+
+  readonly members: MemberActions = {
+    add: this.addMember.bind(this),
+    setMemberType: this.setMemberType.bind(this)
+  };
 }
